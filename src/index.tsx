d1ae0ec5b0420c52ebd01f813a067b4b87236176
hatsu38 <hajiwata0308@gmail.com>
2021-07-16 14:54:49 +0900

    Tailwindcssを導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React, { VFC } from "react";
 import ReactDOM from "react-dom";
 
+import "~/styles/index.scss";
+
+
 export const Index: VFC = () => {
   return (
     <h1>hoge</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
